4d66dcd38d038007095bd66a2cbe564f0e2f348d
tatawarasmi5 <asmitatawar2024.comp@mmcoe.edu.in>
2025-04-12 09:54:53 +0530

    Updates

--- firebase-config.js
+++ firebase-config.js
@@ -9,3 +9,38 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+<div id="membershipBadge"></div>
+
+
+  function loadMembership(userId) {
+    db.collection("memberships").doc(userId).get().then((doc) => {
+      if (doc.exists) {
+        const data = doc.data();
+        let badgeImg = "";
+
+        switch (data.offer) {
+          case "Loyal Paw Prints":
+            badgeImg = "images/ribbon.png";
+            break;
+          case "Adoption Heroes":
+            badgeImg = "images/shield2.png";
+            break;
+          case "Senior Paw Perks":
+            badgeImg = "images/crown.png";
+            break;
+          case "Book 10, Get 10% Off":
+            badgeImg = "images/popper.png";
+            break;
+        }
+
+        document.getElementById("membershipBadge").innerHTML = `
+          <h2 class="text-xl font-bold text-blue-700">Your Membership</h2>
+          <img src="${badgeImg}" alt="Badge" class="w-24 h-24 mx-auto mt-4">
+          <p class="mt-2">${data.offer}</p>
+        `;
+      } else {
+        document.getElementById("membershipBadge").innerText = "You haven’t joined a membership yet.";
+      }
+    });
+  }
